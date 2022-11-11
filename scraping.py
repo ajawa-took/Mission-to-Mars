608a9e99cdcb0cec041387540cdb9d6fb0453cd6
ajawa-took <ajawa_took@yahoo.com>
2022-11-11 00:42:38 -0800

    scraping functionized

--- scraping.py
+++ scraping.py
@@ -4,88 +4,90 @@ from bs4 import BeautifulSoup as soup
 from webdriver_manager.chrome import ChromeDriverManager
 
 import pandas as pd
+import datetime as dt
 
-from time import sleep
-
-# Set up chrome driver, once.
-executable_path = {'executable_path': ChromeDriverManager().install()}
-
-
-#       Get the most recent news bit from one webpage, using splinter and soup
-#       Key output: news_title and news_p strings.
-
-# start browser
-browser = Browser('chrome', **executable_path, headless=False)
-# Visit the mars nasa news site
-url = 'https://redplanetscience.com'
-browser.visit(url)
-# Optional delay for loading the page, checking whether tag exists
-# beyond delay, i don't think we need the next line for the scrape
-browser.is_element_present_by_css('div.list_text', wait_time=100)
-
-# the wait above doesn't seem to work, let's try mimiths
-sleep(50)
-
-# real scrape
-html = browser.html
-news_soup = soup(html, 'html.parser')
-slide_elem = news_soup.select_one('div.list_text')
-
-# Use the parent element to find the first `a` tag and save it as `news_title`
-news_title = slide_elem.find('div', class_='content_title').get_text()
-# news_title
-
-# Use the parent element to find the paragraph text
-news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
-# news_p
-
-# quit and restart browser between webpages, because i get crashes otherwise
-browser.quit()
+def scrape_all():
+    # Initiate headless driver for deployment
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    news_title, news_paragraph = mars_news(executable_path)
+    # Run other two scraping functions, and store all results in dictionary
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(executable_path),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
+    return data
 
 
-#       Get Featured Image from second website, , using splinter and soup again.
-#       Key output: img_url string
+# Get the most recent news bit from one webpage, using splinter and soup
+# output: news_title and news_p strings.
+def mars_news(exepath):
+    # start browser
+    browser = Browser('chrome', **exepath, headless=True)
+    # Visit the mars nasa news site
+    url = 'https://redplanetscience.com'
+    browser.visit(url)
+    # Optional delay for loading the page, checking whether tag exists
+    # beyond delay, i don't think we need the next line for the scrape
+    browser.is_element_present_by_css('div.list_text', wait_time=1)
+    # real scrape
+    html = browser.html
+    news_soup = soup(html, 'html.parser')
+     # Add try/except for error handling
+    try:   
+        slide_elem = news_soup.select_one('div.list_text')
+        # Use the parent element to find the first `a` tag and save it as `news_title`
+        news_title = slide_elem.find('div', class_='content_title').get_text()
+        # Use the parent element to find the paragraph text
+        news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
+    except AttributeError:
+        return None, None
+    # quit and restart browser between webpages, because i get crashes otherwise
+    browser.quit()
+    return news_title, news_p
 
-browser = Browser('chrome', **executable_path, headless=False)
-# Visit URL
-url = 'https://spaceimages-mars.com'
-browser.visit(url)
 
-# Find and click the full image button
-full_image_elem = browser.find_by_tag('button')[1]
-full_image_elem.click()
+# Get Featured Image from second website, , using splinter and soup again.
+# output: img_url string
+def featured_image(exepath):
+    browser = Browser('chrome', **exepath, headless=True)
+    # Visit URL
+    url = 'https://spaceimages-mars.com'
+    browser.visit(url)
+    # Find and click the full image button
+    full_image_elem = browser.find_by_tag('button')[1]
+    full_image_elem.click()
+    # Parse the resulting html with soup
+    html = browser.html
+    img_soup = soup(html, 'html.parser')
+    try:
+        # Find the relative image url
+        img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
+    except AttributeError:
+        return None 
+    # Use the base URL to create an absolute URL
+    # f'{url}/{img_url_rel}'    should also work
+    img_url = f'https://spaceimages-mars.com/{img_url_rel}'
+    browser.quit()
+    return img_url
 
-# Parse the resulting html with soup
-html = browser.html
-img_soup = soup(html, 'html.parser')
 
-# Find the relative image url
-img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
-# img_url_rel
+#  Get a table from third website with pandas.
+# output: string containing html table
+def mars_facts():
+    try:
+        df = pd.read_html('https://galaxyfacts-mars.com')[0]
+    except BaseException:
+        return None
+    df.columns=['description', 'Mars', 'Earth']
+    df.set_index('description', inplace=True)
+    return df.to_html()
 
-# Use the base URL to create an absolute URL
-# f'{url}/{img_url_rel}'    should also work
-img_url = f'https://spaceimages-mars.com/{img_url_rel}'
-# img_url
-
-browser.quit()
-
-
-#       Get a table from third website with pandas.
-#       Key output: mars_fact_table html string
-
-df = pd.read_html('https://galaxyfacts-mars.com')[0]
-df.columns=['description', 'Mars', 'Earth']
-df.set_index('description', inplace=True)
-# df
-mars_fact_table = df.to_html()
-# mars_fact_table
-
-print(news_title)
-print(news_p)
-print( img_url)
-# print(mars_fact_table)
-
+# if __name__ == "__main__":
+#     # If running as script, print scraped data
+#     print(scrape_all())
 
 
 
