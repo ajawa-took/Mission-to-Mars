52ff5a892952d1dcc80d562b710a74295dab993a
ajawa-took <ajawa_took@yahoo.com>
2022-11-11 18:48:02 -0800

    del2 done

--- scraping.py
+++ scraping.py
@@ -10,13 +10,14 @@ def scrape_all():
     # Initiate headless driver for deployment
     executable_path = {'executable_path': ChromeDriverManager().install()}
     news_title, news_paragraph = mars_news(executable_path)
-    # Run other two scraping functions, and store all results in dictionary
+    # Run other 3 scraping functions, and store all results in dictionary
     data = {
         "news_title": news_title,
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(executable_path),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemispheres": hemis(executable_path)
     }
     return data
 
@@ -24,6 +25,7 @@ def scrape_all():
 # I got weird "stale" something, "not attached to document" errors running this
 # then commented everything out, uncommented one line at a time,
 # and now no errors. mega-confusels.
+# oh, i changed headlessness of browser - maybe that fixed error?
 def hemis_test_wrap():
     # Initiate headless driver for deployment
     executable_path = {'executable_path': ChromeDriverManager().install()}
